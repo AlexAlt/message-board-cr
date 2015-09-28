fcd58451c14754a673913f812fed492aa8ed95b6
AlexAlt <alexandra.m.altieri@gmail.com>
2015-09-27 19:33:07 -0700

    Attempt to fix up route handler js/config environment

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 
 export default Ember.Route.extend({
-  model() {
-    return this.store.findAll('question');
-  },
+  // model() {
+  //   return this.store.findAll('question');
+  // }
 });
