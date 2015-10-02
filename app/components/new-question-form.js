f21da17545cbe951f1802db8e597ca1b2b071971
AlexAlt <alexandra.m.altieri@gmail.com>
2015-10-02 11:10:18 -0700

    Allow user to add a new quesquestion

--- app/components/new-question-form.js
+++ app/components/new-question-form.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    saveQuestion(params) {
+      var params = {
+        user: this.get('user'),
+        question: this.get('questionInput'),
+        details: this.get('details'),
+      } 
+      this.sendAction('saveQuestion', params);
+    }
+  }
 });
